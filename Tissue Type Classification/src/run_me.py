00eb3001c8cf78c9a70c339dad3b0b64e63c6a52
David Chong <chongdtwdavid94@gmail.com>
2025-03-09 09:39:20 +0000

    added linear svc for tissue type classificatoin for comparison

--- Tissue Type Classification/src/run_me.py
+++ Tissue Type Classification/src/run_me.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, json
 from sklearn.svm import LinearSVC
 from sklearn.metrics import roc_auc_score, accuracy_score, average_precision_score, f1_score
 
@@ -70,5 +70,48 @@ config["results_processors"] = [lambda x : save_results(x, save_supervised_resul
                                                                           "f1" : lambda ys, preds : f1_score(ys, ((preds >= np.sort(preds, axis=1)[:,[-1]]) & (preds > 0)).astype(int), average="weighted"),
                                                                           "accuracy" : lambda ys, preds : accuracy_score(ys, ((preds >= np.sort(preds, axis=1)[:,[-1]]) & (preds > 0)).astype(int))},
                                                                           "group")]
+gs_params = {"model_params" : {f"c_{c}" : {"estimator" : LinearSVC, "args" : {"C" : c}} for c in [0.001, 0.01, 0.1, 1, 10]}}
+config["grid_search"] = construct_gs_params(gs_params)
 pipeline = MLPipeline(config)
-pipeline.run_crossvalidation()
+#pipeline.run_crossvalidation()
+
+# Compile results
+def compile_results(tag, gridsearch=None):
+    results = []
+    for i, fold in enumerate([x for x in os.listdir(f"{run_dir}/{tag}") if os.path.isdir(f"{run_dir}/{tag}/{x}")]):
+        cvs = [x for x in os.listdir(f"{run_dir}/{tag}/{fold}") if x.find("cv") > -1 and os.path.isdir(f"{run_dir}/{tag}/{fold}/{x}")]
+        if len(cvs) > 1:
+            for cv in cvs:
+                with open(f"{run_dir}/{tag}/{fold}/{cv}/config.txt") as f:
+                    run_config = json.loads(f.read())
+                    result = pd.read_csv(f"{run_dir}/{tag}/{fold}/{cv}/summary_results.csv")
+                    result.columns = ["split"] + list(result.columns[1:])
+                    result["test_fold"] = i
+                    for k,v in gridsearch.items():
+                        result[k] = run_config[v]
+                    results.append(result)
+        else:
+            result = pd.read_csv(f"{run_dir}/{tag}/{fold}/cv_0/summary_results.csv")
+            result.columns = ["split"] + list(result.columns[1:])
+            result["test_fold"] = i
+            results.append(result)
+    results = pd.concat(results)
+    if len(cvs) > 1:
+        top = results.loc[results["split"] == "val"].groupby(list(gridsearch.keys()))["f1"].mean().reset_index().sort_values("f1", ascending=False)
+        filt = None
+        for k,v in gridsearch.items():
+            if filt is None:
+                filt = results[k] == top[k].iat[0]
+            else:
+                filt = (results[k] == top[k].iat[0]) & filt
+        results = results.loc[filt]
+        results = results.groupby("split")[["auc", "auprc", "f1", "accuracy"]].agg(["mean", "std"])
+        hyperparams = "_".join([top[k].iat[0] for k in gridsearch.keys()])
+        results.to_csv(f"{wd}/{tag}_{hyperparams}_results.csv")
+    else:
+        results = results.groupby("split")[["auc", "auprc", "f1", "accuracy"]].agg(["mean", "std"])
+        results.to_csv(f"{wd}/{tag}_results.csv")
+
+compile_results("pnet")
+compile_results("dense")
+compile_results("svc", {"c" : "model_params_choice"})
