54103043ddc44f97b2aa372f65306441133721c7
James Lindsey-Jones <james.lindseyjones@gmaik.com>
2026-06-04 16:15:28 +0100

    prostate prediction: alter CM style

--- prostate_cancer_prediction/plotting/plot_external_validation.py
+++ prostate_cancer_prediction/plotting/plot_external_validation.py
@@ -1,6 +1,9 @@
+import itertools
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from sklearn.metrics import confusion_matrix
 
 
@@ -19,14 +22,50 @@ def plot_external_validation(run_dir, figures_dir):
 
     conf_mat = confusion_matrix(all_true, (all_preds > 0.5).astype(int), normalize="true") * 100
 
-    # Fix label indexing: conf_mat[row=actual, col=predicted], so labels[row, col]
-    labels = np.array([["TN: ", "FP: "], ["FN: ", "TP: "]])
-    plt.imshow(conf_mat)
-    plt.xticks(ticks=[0, 1], labels=["Localised", "Metastatic"])
-    plt.yticks(ticks=[], labels=[])
-    for i in range(2):
-        for j in range(2):
-            plt.text(j - 0.25, i, f"{labels[i, j]}{round(conf_mat[i, j], 2)}%")
-    plt.colorbar()
-    plt.savefig(f"{figures_dir}/pnet_external_validation.jpg")
+    fig = plt.figure(figsize=(4, 4))
+    ax = fig.subplots(1, 1)
+    _plot_confusion_matrix(ax, conf_mat)
+    plt.savefig(f"{figures_dir}/pnet_external_validation.jpg", dpi=200)
     plt.close()
+
+
+def _plot_confusion_matrix(ax, conf_mat):
+    cmap = plt.cm.Reds
+
+    # Columns = true class (dataset), rows = predicted, so each dataset column
+    # sums to 100%. conf_mat is row=actual, col=predicted, so transpose.
+    cm = conf_mat.T
+    labels = np.array([["TN", "FN"], ["FP", "TP"]])
+    classes = ["localized", "metastatic"]
+
+    im = ax.imshow(cm, interpolation="nearest", cmap=cmap)
+    fig = ax.get_figure()
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="10%", pad=0.1)
+    cb = fig.colorbar(im, cax=cax, orientation="vertical")
+    cax.yaxis.set_ticks_position("right")
+    cb.outline.set_visible(False)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        ax.text(j, i, "{}: {:.2f}%".format(labels[i, j], cm[i, j]),
+                horizontalalignment="center",
+                color="white" if cm[i, j] > thresh else "black", fontsize=12)
+
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+
+    tick_marks = np.arange(len(classes))
+    ax.set_xticks(tick_marks)
+    ax.set_xticklabels(classes, rotation=0)
+    ax.set_yticks([])
+
+    ax2 = divider.append_axes("bottom", size="5%", pad=0.6)
+    ax2.spines["top"].set_visible(False)
+    ax2.spines["right"].set_visible(False)
+    ax2.spines["left"].set_visible(False)
+    ax2.spines["bottom"].set_visible(False)
+    ax2.set_yticks([])
+    ax2.set_xticks([])
