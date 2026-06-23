dce07d0fbd10c856bf407a183da47dc663cf91f9
James Lindsey-Jones <james.lindseyjones@gmaik.com>
2026-06-23 10:21:08 +0100

    glioma hyperparam sensitivity analysis

--- glioma_prediction/run.py
+++ glioma_prediction/run.py
@@ -4,7 +4,7 @@ import sys
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, project_root)
 
-from glioma_prediction.configs.pnet import pnet_config
+from glioma_prediction.configs.pnet import pnet_config, n_hidden_layers
 from glioma_prediction.configs.pnet_GO import pnet_GO_config
 from glioma_prediction.configs.pnet_hyperparameter_sensitivity import pnet_hyperparameter_sensitivity_config
 from glioma_prediction.configs.dense import dense_config
@@ -21,6 +21,7 @@ from glioma_prediction.configs.base_config import run_dir, figures_dir, wd
 from glioma_prediction.plotting.plot_nested_cv import plot_nested_CV
 from glioma_prediction.plotting.plot_external_validation import plot_external_validation
 from glioma_prediction.plotting.plot_hyperparameter_sensitivity import analyse as plot_hyperparameter_sensitivity
+from prostate_cancer_prediction.plotting.plot_sankey import plot_sankey
 from glioma_prediction.significance_testing import significance_test
 
 
@@ -46,6 +47,13 @@ def run():
     plot_nested_CV(run_dir, figures_dir)
     plot_external_validation(run_dir, figures_dir)
     plot_hyperparameter_sensitivity()
+
+    # Sankey diagram of the first outer split's P-NET model.
+    pnet_run_dir = f"{run_dir}/pnet/test_0/best_auc"
+    dataset_id_mappings = "architecture/Reactome/ReactomePathways.txt"
+    plot_sankey(pnet_run_dir, n_hidden_layers, figures_dir, dataset_id_mappings,
+                output_prefix="pnet_single_split")
+
     significance_test(run_dir, wd)
 
 
