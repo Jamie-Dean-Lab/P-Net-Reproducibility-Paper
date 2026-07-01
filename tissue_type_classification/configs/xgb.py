e1e849e91f3964794bed24cfd437b919d47982fa
jameslindsey-jones <james.lindseyjones@gmail.com>
2026-07-01 11:51:42 +0100

    update xgb config

--- tissue_type_classification/configs/xgb.py
+++ tissue_type_classification/configs/xgb.py
@@ -15,10 +15,10 @@ xgb_config = {
     "run_method":         "run_crossvalidation",
     "grid_search": {
         "model_params": {
-            f"lr_{lr}_depth_{d}_mcw_{mcw}_n_{n}": {
+            f"lr_{lr}_depth_{d}_mcw_{mcw}": {
                 "estimator": XGBClassifier,
                 "args": {
-                    "n_estimators": n,
+                    "n_estimators": 100,
                     "learning_rate": lr,
                     "max_depth": d,
                     "min_child_weight": mcw,
@@ -26,10 +26,9 @@ xgb_config = {
                     "verbosity": 0,
                 },
             }
-            for lr in [0.01, 0.05, 0.1, 0.3, 1]
-            for d in [3, 6, 9]
+            for lr in [0.01, 0.1, 1]
+            for d in [3, 6]
             for mcw in [1, 10]
-            for n in [50, 100]
         }
     },
 }
